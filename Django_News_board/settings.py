ee90c4120c520b6e97e1d19644341641e70b71d2
Nick Letunovski <nletunovski@Nicks-MacBook-Pro.local>
2021-05-10 15:36:00 +0300

    heroku upload fix

--- Django_News_board/settings.py
+++ Django_News_board/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     "news_board",
 ]
 
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 CRONJOBS = [
     ('58 23 * * *', 'news_board.cron.clean_upvotes_job')
